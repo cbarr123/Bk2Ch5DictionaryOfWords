8f683d05883dbe923118b476128a3f273cf49904
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-10-04 15:56:30 -0500

    adding new dictionaries

--- Program.cs
+++ Program.cs
@@ -14,17 +14,23 @@ namespace DictionaryOfWords
             wordsAndDefinitions.Add("Awesome", "The feeling of students when they are learning C#");
             wordsAndDefinitions.Add("Concerned", "The feeling of students when they don't understand C#");
             wordsAndDefinitions.Add("Hungry", "The feeling of students at 11:45");
-
             /*
                 Use square brackets to get the definition of two of the
                 words and then output them to the console
             */
-            // Console.WriteLine(wordsAndDefinitions[1].Key);
-
             foreach(KeyValuePair<string, string> word in wordsAndDefinitions) 
             {
-               Console.WriteLine($"The definition of {word.Key} is {word.Value} "); 
+               Console.WriteLine($"The definition of {word.Key} is {word.Value}."); 
             }
+
+            // Make a new list 
+            Console.WriteLine(); 
+            
+            List<Dictionary<string, string>> dictionaryOfWords = new List<Dictionary<string, string>>();
+            Dictionary<string, string> excitedWord = new Dictionary<string, string>();
+            excitedWord.Add("word", "excited");
+
+
 
         }
     }
